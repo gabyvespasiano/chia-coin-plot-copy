14bcd39cf8770236e742bfde4e29369606318dc2
gabyvespasiano <gabyvespasiano@gmail.com>
2023-02-02 21:15:56 -0300

    se agregaron comentarios que explican el codigo mejor

--- copy.py
+++ copy.py
@@ -18,22 +18,22 @@ import time
 import _thread
 
 
+discos = ["/media/server/4tb 22/plots","/media/server/4tb 23/plots","/media/server/8 tb2/plots"] # Ubicación de los discos
+plots = [19,7,73] # Número de plots faltantes en cada disco 
+plotsR = [] # Contador de plots copiados
+plotsD = [] # Indicador de estado de copia (0=ocupado, 1=libre)
 
-
-discos = ["/media/server/4tb 22/plots","/media/server/4tb 23/plots","/media/server/8 tb2/plots"] ## introducir en orden las ubicaciones de los discos
-plots = [19,7,73]## aqui hay que introducir en orden la cantidad de plots faltantes en los discos 
-plotsR = []
-plotsD = []
-
+# Inicializa los contadores y estados de copia
 for x in plots:
     plotsR.append(0)
     plotsD.append(1)
 
-disco = 0
-path = "/media/server/destino/"## aqui introducir donde se estaran generando los plots que deberan ser copiados
-x = ""
+disco = 0 # Índice del disco actual
+path = "/media/server/destino/" # Directorio de origen de los plots a copiar
+x = "" # Nombre del archivo de plot actual
 
 def copia():
+	# Variables locales
 	global x
 	global disco
 	global discos
@@ -45,52 +45,66 @@ def copia():
 	tx = x
 	tdisco = disco
 	tdiscos = discos
+	
+	# Cambia el estado de la copia actual a ocupado
 	plotsD[tdisco] = 0
 	plotsR[disco] = plotsR[disco] + 1
-	print("\033[4;35m"+"copia iniciada "+str(tplotsR[tdisco]) +"/"+str(tplots[tdisco])+" en " + tdiscos[tdisco] + " de " + tx)
-	os.rename(path+"/"+x, path+"/"+x+"C")
-	#os.replace(path+"/"+x, "J:/disco1/"+x)
-	shutil.move(path+"/"+x+"C", discos[disco]+"/"+x)
-	print("\x1b[1;33m"+"copia termianda "+str(tplotsR[tdisco]) +"/"+str(tplots[tdisco])+" en " + tdiscos[tdisco] + " de " + tx)
+	
+	# Imprime el inicio de la copia
+	print("\033[4;35m" + "Copia iniciada " + str(tplotsR[tdisco]) + "/" + str(tplots[tdisco]) + " en " + tdiscos[tdisco] + " de " + tx)
+	
+	# Realiza la copia renombrándolo primero
+	os.rename(path + "/" + x, path + "/" + x + "C")
+	shutil.move(path + "/" + x + "C", discos[disco] + "/" + x)
+	
+	# Imprime la finalización de la copia
+	print("\x1b[1;33m" + "Copia terminada " + str(tplotsR[tdisco]) + "/" + str(tplots[tdisco]) + " en " + tdiscos[tdisco] + " de " + tx)
+	
+	# Cambia el estado de la copia actual a libre
 	plotsD[tdisco] = 1
+	
+	# Sale del hilo
 	_thread.exit()
 
 plotea = False
+
+# Bucle infinito
 while True:
-	dir_list = os.listdir(path)
+    dir_list = os.listdir(path)
 
-	#print("Files and directories in '", path, "' :")
- 
-	# prints all files
-	for x in dir_list:
-		if x.endswith(".plot"):
-			#print(x)
-			plotea = False
-			while plotea == False:		
-				#print(disco)			
-				if plots[disco] <= plotsR[disco]:
-					disco = disco + 1
-					if disco >= len(discos):
-						disco = 0
-				else:
-					if plotsD[disco] == 0:
-						disco = disco + 1
-						if disco >= len(discos):
-							disco = 0
-						time.sleep(5)
-					else:		
-						plotea = True
-				
-			
-			_thread.start_new_thread(copia,())
+    # Obtiene la lista de archivos y directorios en el path especificado
+    for x in dir_list:
+        # Verifica si el archivo termina con ".plot"
+        if x.endswith(".plot"):
+            plotea = False
+            # Busca un disco disponible para copiar el archivo
+            while plotea == False:       
+                if plots[disco] <= plotsR[disco]:
+                    # Si se han copiado todos los archivos posibles en el disco actual, se cambia al siguiente
+                    disco = disco + 1
+                    if disco >= len(discos):
+                        disco = 0
+                else:
+                    if plotsD[disco] == 0:
+                        # Si el disco actual está ocupado, se cambia al siguiente
+                        disco = disco + 1
+                        if disco >= len(discos):
+                            disco = 0
+                        time.sleep(5)
+                    else:
+                        # Si el disco actual está disponible, se puede copiar el archivo
+                        plotea = True
+                        
+            # Inicia una nueva hebra para copiar el archivo
+            _thread.start_new_thread(copia,())
 
-			time.sleep(5)
-			disco = disco + 1
-			if disco >= len(discos):
-				disco = 0
-		time.sleep(10)
-			
-#print(dir_list[0])
-	time.sleep(900)
-	#if os.path.exists('G:\plots\*.plot'):
-	#    print("existe")
+            # Espera 5 segundos antes de continuar con la siguiente iteración
+            time.sleep(5)
+            # Cambia al siguiente disco
+            disco = disco + 1
+            if disco >= len(discos):
+                disco = 0
+        # Espera 10 segundos antes de revisar nuevamente la carpeta
+        time.sleep(10)
+    # Espera 15 minutos antes de revisar nuevamente la carpeta
+    time.sleep(900)
